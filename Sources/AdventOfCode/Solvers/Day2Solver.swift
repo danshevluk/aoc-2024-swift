7a51f12e1cb3eb4d9999b58688115e4969e2788a
Dan Shevluk <dan.shevluk@hey.com>
2024-12-03 00:02:32 +0000

    Day 2, part 2

--- Sources/AdventOfCode/Solvers/Day2Solver.swift
+++ Sources/AdventOfCode/Solvers/Day2Solver.swift
@@ -8,25 +8,28 @@ struct Day2Solver: PuzzleSolver {
         }
     }
 
-    func solvePart1(_ inputData: String) -> String? {
-        let lines = inputData.components(separatedBy: .newlines)
-
-        func isDataSafe(_ levels: [Int]) -> Bool {
-            var increasing: Bool?
-            for i in 0..<levels.count-1 {
-                let diff = levels[i + 1] - levels[i]
-                defer { increasing = diff > 0 }
-
-                let isConsistent = increasing.map { $0 == (diff > 0) } ?? true
-                let diffInBounds = (1...3) ~= abs(diff)
-
-                if !isConsistent || !diffInBounds {
-                    return false
+    func isDataSafe(_ levels: [Int]) -> Bool {
+        var isIncreasing: Bool?
+        for i in 0..<levels.count-1 {
+            let diff = levels[i + 1] - levels[i]
+            defer {
+                if isIncreasing == nil {
+                    isIncreasing = diff > 0
                 }
             }
-            return true
-        }
 
+            let isConsistent = isIncreasing.map { $0 == (diff > 0) } ?? true
+            let diffInBounds = (1...3) ~= abs(diff)
+
+            if !isConsistent || !diffInBounds {
+                return false
+            }
+        }
+        return true
+    }
+
+    func solvePart1(_ inputData: String) -> String? {
+        let lines = inputData.components(separatedBy: .newlines)
         let safeLines = lines.reduce(into: 0) { result, line in
             let levels = line.components(separatedBy: .whitespaces).compactMap { Int($0) }
             let isSafe = isDataSafe(levels)
@@ -39,6 +42,22 @@ struct Day2Solver: PuzzleSolver {
     }
 
     private func solvePart2(_ inputData: String) -> String? {
-        return nil
+        let lines = inputData.components(separatedBy: .newlines)
+
+        let safeLines = lines.reduce(into: 0) { result, line in
+            let levels = line.components(separatedBy: .whitespaces).compactMap { Int($0) }
+
+            for skip in 0..<levels.count {
+                var modifiedLevels = levels
+                modifiedLevels.remove(at: skip)
+
+                let isSafe = isDataSafe(modifiedLevels)
+                if isSafe {
+                    result += 1
+                    break
+                }
+            }
+        }
+        return String(safeLines)
     }
 }
